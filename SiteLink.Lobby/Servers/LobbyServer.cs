5d86aea7da02b993b9fa8d0a7e727c7a5f1f55b2
Killers <killers0992@gmail.com>
2025-11-01 22:10:52 +0100

    Add project files.

---
+++ SiteLink.Lobby/Servers/LobbyServer.cs
@@ -0,0 +1,15 @@
+﻿using Lobby.Worlds;
+using SiteLink.API.Core;
+using SiteLink.API.Networking;
+
+namespace Lobby;
+
+public class LobbyServer : Server
+{
+    public LobbyServer() : base(null, "Lobby", "-lobby-", 7777, true, false) { }
+
+    public override bool OnClientConnecting(Client client) => true;
+    public override void OnClientConnected(Client client) => client.SendToScene("Facility");
+    public override void OnClientReady(Client client) => client.SpawnObjects();
+    public override void OnClientSpawnPlayer(Client client) => client.World = new LobbyWorld();
+}
---
+++ SiteLink.Lobby/Worlds/LobbyWorld.cs
@@ -0,0 +1,67 @@
+﻿using Lobby.Models;
+using PlayerRoles;
+using Portals.Core;
+using SiteLink.API.Core;
+using SiteLink.API.Networking;
+using SiteLink.API.Networking.Objects;
+using UnityEngine;
+
+namespace Lobby.Worlds;
+
+public class LobbyWorld : World
+{
+    //public ConfigSyncObject ConfigSync;
+    public WaypointToyObject Waypoint;
+    public TextToyObject TextToy;
+    public TextToyObject TextToy2;
+
+    public LobbyWorld() : base("Lobby")
+    {
+        DestroyOnEmpty = true;
+
+        AddWaypoint(new Vector3(0f, -300f, 0f));
+
+        //ConfigSync = new ConfigSyncObject(this);
+
+        foreach (TextInfo text in MainClass.Singleton.Config.Texts)
+        {
+            TextToyObject textObject = new TextToyObject(this)
+            {
+                Position = new Vector3(text.PositionX, text.PositionY, text.PositionZ),
+            };
+
+            textObject.TextToy.TextFormat = text.Text;
+            textObject.TextToy.DisplaySize = new Vector2(150f, 25f);
+        }
+
+        foreach (PortalInfo portal in MainClass.Singleton.Config.Portals)
+        {
+            new Portal(this, portal.TargetServer, portal.Text, new Vector3(portal.PositionX, portal.PositionY, portal.PositionZ), new Quaternion(0f, portal.Rotation, 0f, 0f));
+        }
+    }
+
+    public override void Update()
+    {
+        PortalController.Update(this);
+    }
+
+    public override void OnLoad(Client client)
+    {
+        client.SpawnPlayer();
+    }
+
+    public override void OnObjectsSpawned(Client client)
+    {
+        client.SetRole(RoleTypeId.Tutorial);
+        client.SetHealth(100f);
+        client.SetSeed(350);
+
+        client.Object.PlayerAuthenticationManager.SyncedUserId = client.PreAuth.UserId;
+        client.Object.SendUpdate(client);
+    }
+
+    public override void OnDestroy()
+    {
+        Portal.SpawnedPortals.Remove(this);
+    }
+}
